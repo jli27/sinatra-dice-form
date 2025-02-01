60a4e65cdf74d7aad3dc89dc11fb1b78e9447188
jli27 <96714100+jli27@users.noreply.github.com>
2025-02-01 23:42:20 +0000

    finished

--- app.rb
+++ app.rb
@@ -2,8 +2,21 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/process_roll") do
+  @num_dice = params.fetch("num_dice").to_i
+  @sides = params.fetch("sides").to_i
+  @process_roll = "/process_roll?dice=#{@num_dice}&sides=#{@sides}"
+
+  @rolls = []
+
+  @num_dice.times do
+    die = rand(1..@sides)
+
+    @rolls.push(die)
+  end
+
+  erb(:flexible)
 end
