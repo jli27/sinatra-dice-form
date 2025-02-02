7aab6bb63ddacde3e7ed599edb624d602be205c5
jli27 <96714100+jli27@users.noreply.github.com>
2025-02-02 22:41:53 +0000

    fixed....

--- app.rb
+++ app.rb
@@ -6,13 +6,13 @@ get("/") do
 end
 
 get("/process_roll") do
-  @num_dice = params.fetch("num_dice").to_i
+  @dice = params.fetch("dice").to_i
   @sides = params.fetch("sides").to_i
-  @process_roll = "/process_roll?dice=#{@num_dice}&sides=#{@sides}"
+  @process_roll = "/process_roll?dice=#{@dice}&sides=#{@sides}"
 
   @rolls = []
 
-  @num_dice.times do
+  @dice.times do
     die = rand(1..@sides)
 
     @rolls.push(die)
